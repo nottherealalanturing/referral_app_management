f70ccfbfa1672aed7583aa3fc9f49a9a7b326bcd
ahmad <abdulkareemahmad413@gmail.com>
2024-08-05 22:40:14 +0100

    succesfully completed my javascript modification

--- scripts/users.js
+++ scripts/users.js
@@ -1,5 +1,11 @@
 const chats = document.querySelector (".chatsBtn");
 const business = document.querySelector (".business-name");
 const status = document.querySelector (".users-status");
-const chatDiv = document.querySelector ("chatSection");
 const modal = document.querySelector (".modal-bg");
+const activityDiv = document.querySelector (".activityTable");
+const chatDiv = document.querySelector (".chatSection");
+
+chats.addEventListener ("click", function (){
+    chatDiv.style.display = "block";
+    activityDiv.style.display = "none";
+});
